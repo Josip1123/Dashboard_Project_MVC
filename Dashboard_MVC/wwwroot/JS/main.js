e7ae309d623405dff116f894a664aa789a19b8b7
Josip1123 <jopetk@hotmail.com>
2025-04-27 23:05:38 +0200

    create and remove cookie functionality with banner

--- Dashboard_MVC/wwwroot/JS/main.js
+++ Dashboard_MVC/wwwroot/JS/main.js
@@ -54,47 +54,79 @@ document.addEventListener('DOMContentLoaded', function() {
                 window.location.href = '/Dashboard/Dashboard';
             });
         }
+
+    const listFilters = document.querySelectorAll(".list-filter");
+    const dashboardCards = document.querySelectorAll(".dashboard-card");   
+       
+    if(listFilters){
         
-        
-        /*List Filter
-        const listFilters = document.querySelectorAll(".list-filter");
-        const dashboardCards = document.querySelectorAll(".dashboard-card");
-        
-        listFilters.forEach(filter =>{
-            filter.addEventListener("click", ()=>{
-                
-                listFilters.forEach(item =>{
-                    item.classList.remove("filter-active")
-                });
-                filter.classList.add("filter-active");
-            
-            const filterType = filter.dataset.filter.toLowerCase().trim();
-                
-            dashboardCards.forEach(card => {
-                const isCompleted = card.dataset.status.toLocaleLowerCase().trim() === "true";
-                console.log(isCompleted);
-                console.log(filterType);
-                
-                if (filterType === "completed" && !isCompleted){
-                    card.classList.add("hidden")
-                } else if (filterType === "started" && isCompleted) {
-                    card.classList.add("hidden")
-                } else {
-                    card.classList.remove("hidden")
-                }
-            })
-        })
-        })
-        
-        
-         */
+           listFilters.forEach(filter =>{
+               filter.addEventListener("click", ()=>{
+
+                   listFilters.forEach(item =>{
+                       item.classList.remove("filter-active")
+                   });
+                   filter.classList.add("filter-active");
+
+                   const filterType = filter.dataset.filter.toLowerCase().trim();
+
+                   dashboardCards.forEach(card => {
+                       const isCompleted = card.dataset.status.toLocaleLowerCase().trim() === "true";
+                       console.log(isCompleted);
+                       console.log(filterType);
+
+                       if (filterType === "completed" && !isCompleted){
+                           card.classList.add("hidden")
+                       } else if (filterType === "started" && isCompleted) {
+                           card.classList.add("hidden")
+                       } else {
+                           card.classList.remove("hidden")
+                       }
+                   })
+               })
+           }) 
+       }
+       
     
     
     /*Cookie consent*/
 
-    const consentBtn = document.querySelector(".accept-policy");
-    consentBtn.addEventListener("click", ()=>{
-        document.cookie = consentBtn.dataset.cookieString;
-    } )
+    const consentBtn = document.querySelector(".accept-cookie");
+    const banner = document.querySelector(".cookie-box");
+    const declineBtn = document.querySelector(".decline-cookie");
+    const cookieReviewBtn= document.querySelector(".cookie-review");
+    
+    if (consentBtn) {
+        consentBtn.addEventListener("click", () => {
+            document.cookie = consentBtn.dataset.cookieString;
+            if (banner) {
+                banner.classList.add("hidden");
+            }
+            if (cookieReviewBtn) {
+                cookieReviewBtn.classList.remove("hidden");
+            }
+        });
+    }
+
+    
+    if (declineBtn) {
+        declineBtn.addEventListener("click", () => {
+            document.cookie = ".AspNet.Consent=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+            if (banner) {
+                banner.classList.add("hidden");
+            }
+            if (cookieReviewBtn) {
+                cookieReviewBtn.classList.remove("hidden");
+            }
+        });
+    }
+    
+    
+    if (cookieReviewBtn) {
+        cookieReviewBtn.addEventListener("click", () => {
+            banner.classList.remove("hidden");
+            cookieReviewBtn.classList.add("hidden");
+        })
+    }
 });
 
